dbc7621ffa2b5fa61b2b95812298a2a388a42386
patrickreinan <patrickreinan@gmail.com>
2022-11-18 10:37:03 -0300

    first commit

---
+++ create-env.sh
@@ -0,0 +1,15 @@
+CONTEXT=camelk
+
+minikube -p $CONTEXT start --cpus=2 --memory=4096
+minikube -p $CONTEXT addons enable registry
+export REGISTRY_ADDRESS=$(kubectl -n kube-system get service registry -o jsonpath='{.spec.clusterIP}')
+
+
+
+helm repo add camel-k https://apache.github.io/camel-k/charts
+helm install \
+  --generate-name \
+  --set platform.build.registry.address=${REGISTRY_ADDRESS} \
+  --set platform.build.registry.insecure=true \
+  --set platform.cluster=kubernetes \
+  camel-k/camel-k
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Creating manifest file
+
+```sh
+kamel run test.java -o yaml > test.yaml
+```
+
+## Apply manifest
+```sh
+kubectl apply -f test.yaml
+```
+
